a381adbabcbff53a58423cb43c34d284587f62d7
jinsujin <sylpid003@gmail.com>
2020-02-07 22:45:45 +0900

    add local project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <p>react peoject</p>
+        </div>
+    );
+};
+
+export default App;
+
+ReactDOM.render(<App/>, document.getElementById("app"));
